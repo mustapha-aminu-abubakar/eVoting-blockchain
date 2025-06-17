6907699d966785d9523849448c56148dd11fcfa5
Mustapha Aminu <114070811+Amustee@users.noreply.github.com>
2025-06-17 12:02:48 +0100

    documenting

--- dapp/admin.py
+++ dapp/admin.py
@@ -1,4 +1,4 @@
-
+import sys
 
 from flask import Blueprint, flash, redirect, render_template, request, url_for, session
 from flask_login import current_user, login_required
@@ -14,7 +14,7 @@ from .db_operations import (ban_candidate_by_id, ban_voter_by_id,
                             fetch_all_votes, fetch_all_transactions)
 from .ethereum import Blockchain
 from .role import ElectionStatus
-from .models import Candidate, Voter
+from .models import Candidate, Voter, Vote, Result
 from .db import database
 from .validator import (convert_to_unix_timestamp, count_max_vote_owner_id,
                         count_total_vote_cast, is_admin, sha256_hash,
@@ -27,134 +27,105 @@ admin = Blueprint('admin', __name__)
 @admin.route('/admin_panel')
 @login_required
 def admin_panel():
-    'Shows the admin panel'
+    """
+    Renders the admin panel with election, voter, candidate, and transaction data.
 
-    # Access deny for other
+    Only accessible to admin users. Fetches all relevant information from the database and blockchain,
+    then renders the admin panel template with this data.
+    """
+
+    # Access deny for non-admin users
     if not is_admin(current_user):
         return redirect(url_for('auth.index'))
 
+    # Create a blockchain object
     blockchain = Blockchain(
         fetch_admin_wallet_address(),
         fetch_contract_address()
     )
-    # Fetch all information
+    # Fetch important information
     election = fetch_election()
     voters = fetch_all_voters()
-    # candidates = fetch_election_result_restricted()
     total_votes_possible = count_total_possible_votes()
-    # total_votes_cast = blockchain.get_all_votes()
-    votes = fetch_all_votes
+    votes = fetch_all_votes()
     candidates = fetch_all_candidates()
     positions = fetch_all_positions()
     txns = fetch_all_transactions()
-
-    # How many voted
-    # total_vote_cast = count_total_vote_cast(voters)
-    # Max vote and IDs
-    # total_vote_count, max_vote_owner_id = count_max_vote_owner_id(candidates)
 
     return render_template(
         'admin_panel.html',
         election_status=election.status,
         candidates=candidates,
         txns=txns,
-        # total_vote_count=len(votes),
         total_votes_possible=total_votes_possible,
-        # positions_count=len(positions),
         votes=votes,
         ts_to_dt=datetime.utcfromtimestamp
-        # total_vote_cast=total_vote_cast
     )
-
-
-# @admin.route('/publish')
-# @login_required
-# def publish():
-#     'Publish / Rollback election result'
-
-#     # Access deny for other
-#     if not is_admin(current_user):
-#         return redirect(url_for('auth.index'))
-
-#     blockchain = Blockchain(
-#         fetch_admin_wallet_address(),
-#         fetch_contract_address()
-#     )
-
-#     candidates = fetch_election_result()
-#     for candidate in candidates:
-#         voters = fetch_voters_by_candidate_id(candidate.id)
-#         # print(voters)
-
-#         # Checking vote counts
-#         if candidate.vote_count != len(voters):
-#             flash(f'Results tampered for {candidate.name}')
-#             return render_template('error.html', error_msg='Vote count mismatch')
-
-#         # Get the hash from the blokchain
-#         voteHash_from_blockchain = blockchain.get_hash_by_candidate_hash(
-#             sha256_hash(candidate.username)
-#         )
-
-#         # Validate the computed hash and bash from blockchain
-#         if not validate_result_hash(voters, voteHash_from_blockchain):
-#             flash(f'Results tampered for {candidate.name}')
-#             return render_template('error.html', error_msg='Voting hash mismatch')
-
-#     election = publish_result()
-
-#     # Notify current status
-#     if election.status == ElectionStatus.PUBLIC:
-#         flash('Election result is now public')
-#     else:
-#         flash('Election result is now private')
-
-#     return redirect(url_for('admin.admin_panel'))
 
 @admin.route('/publish')
 @login_required
 def publish_results():
+    """
+    Publishes the election results to the blockchain and displays grouped results.
+
+    Only accessible to admin users. Calls the blockchain to publish results, fetches and prints grouped results and votes,
+    and handles exceptions by flashing error messages.
+    """
+    # Access denied for non-admin users
     if not is_admin(current_user):
         return redirect(url_for('auth.index'))
-    results=[]
-    votes=[]
     try:
-        blockchain = Blockchain(
-            fetch_admin_wallet_address(),
+        blockchain = Blockchain( # Create Blockchain object
+            fetch_admin_wallet_address(), 
             fetch_contract_address()
         )
-        status, tx_receipt = blockchain.publish() 
+        status, tx_receipt = blockchain.publish() # Publish on-chain election results
     except Exception as e:
         flash(str(e), 'error')
-    if status:       
-        results = blockchain.group_candidates_by_position()
-        votes = blockchain.get_all_votes()    
-
-    print(f'Results:\n {results} \n')
-    print(f'Votes:\n {votes} \n')
+        status = False
     
-    try:
-        add_results(results)
-    except Exception as e:
-        print(f'Could not enter results: {e}')
+    # If publish successful, fetch election aggregated results and all votes cast
+    if status:       
+        results = blockchain.group_candidates_by_position() 
+        votes = blockchain.get_all_votes()   
+
+        print(f'Fetched results for {len(results)} positions')
+        print(f'Fetched {len(votes)} votes')
         
-    try: 
-        add_votes(votes)    
-    except Exception as e:
-        print(f'Could not enter votes: {e}')
-        
+        try: # Adds aggregated results to local database
+            sys.stdout.write('Writing results to database ...   \n')
+            add_results(results)
+        except Exception as e:
+            print(f'Could not enter results: {e}')
             
-        # Voter.lock_all(database.session)
-        print(f'Votes model is_locked: {Voter.is_locked(database.session)}')
+        try: # Adds fetched votes to local database
+            sys.stdout.write('Adding votes to database ...   \n')
+            add_votes(votes)    
+        except Exception as e:
+            print(f'Could not enter votes: {e}')
+        
+        # Locks Voter, Vote and Result models, preventing manipulation
+        Voter.lock_all(database.session)
+        Vote.lock_all(database.session)
+        Result.lock_all(database.session)
+        sys.stdout.write(f'Voter model is_locked: {Voter.is_locked(database.session)}')
+        sys.stdout.write(f'Votes model is_locked: {Vote.is_locked(database.session)}')
+        sys.stdout.write(f'Result model is_locked: {Result.is_locked(database.session)}')
         flash(f"Results published. Tx: {tx_receipt}")
     
-    return redirect(url_for('admin.publish_results'))
+    return redirect(url_for('admin.admin_panel'))
 
 @admin.route('/block_candidate/<int:candidate_id>')
 @login_required
 def block_candidate(candidate_id):
-    'Block / Unblock candidate'
+    """
+    Blocks or unblocks a candidate by their ID.
 
+    Only accessible to admin users. Toggles the candidate's status and flashes a message indicating the new status.
+    
+    Args:
+        candidate_id (int): The ID of the candidate to block or unblock.
+    """
     # Access deny for other
     if not is_admin(current_user):
         return redirect(url_for('auth.index'))
@@ -169,7 +140,14 @@ def block_candidate(candidate_id):
 @admin.route('/block_voter/<int:voter_id>')
 @login_required
 def block_voter(voter_id):
-    'Block / Unblock voter'
+    """
+    Blocks or unblocks a voter by their ID.
+
+    Only accessible to admin users. Toggles the voter's status and flashes a message indicating the new status.
+    
+    Args:
+        voter_id (int): The ID of the voter to block or unblock.
+    """
 
     # Access deny for other
     if not is_admin(current_user):
@@ -185,8 +163,11 @@ def block_voter(voter_id):
 @admin.route('/update_time', methods=['POST'])
 @login_required
 def update_time_post():
-    'Extend the end time of the election'
+    """
+    Extends or updates the end time of the election.
 
+    Only accessible to admin users. Processes form data to update the election's start and end times on the blockchain.
+    """
     # Access deny for other
     if not is_admin(current_user):
         return redirect(url_for('auth.index'))
@@ -197,7 +178,7 @@ def update_time_post():
     tz = request.form.get('tz').strip()
     private_key = request.form.get('private_key').strip()
     
-    print(f'[flask UI] Start time: {start_time}, End time: {end_time}, timezone: {tz}')
+    print(f'[flask UI] Start time: {datetime.fromtimestamp(start_time, tz=tz)}, End time: {datetime.fromtimestamp(end_time, tz=tz)}, timezone: {tz}')
 
     blockchain = Blockchain(
         fetch_admin_wallet_address(),
@@ -205,6 +186,13 @@ def update_time_post():
     )
 
     def show_flash_msg(status, tx_msg):
+        """
+        Displays a flash message indicating the status of a blockchain transaction.
+
+        Args:
+            status (bool): Whether the transaction was successful.
+            tx_msg (str): The transaction hash or message.
+        """
         if status:
             flash(f'[Updated] Tx HASH: {tx_msg}')
         else:
@@ -212,7 +200,7 @@ def update_time_post():
 
     if not private_key:
         flash('Private key empty')
-
+        return redirect(url_for('admin.admin_panel'))
     elif start_time and end_time:
         # Sending transaction for setting start and end time of election
         status, tx_msg = blockchain.set_voting_time(
